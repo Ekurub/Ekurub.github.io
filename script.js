38ebac154388f11b73a6e7d813d362a6fe3d4337
Buruke Gebregziabher <gburuke@gmail.com>
2024-02-15 00:52:07 -0600

    made my website

--- script.js
+++ script.js
@@ -7,14 +7,20 @@ const noMessages = [
    "Come on ChowChau" ]
 
 
-const newimage = "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExamx5M2FrYTQzZWFlaDM3ajloa2lxanEwZnl5bzBtaXQyZnl2ZTZnaiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/GwskZm1jXg8cDvuZJ6/giphy.gif"
+const noImage = "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExamx5M2FrYTQzZWFlaDM3ajloa2lxanEwZnl5bzBtaXQyZnl2ZTZnaiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/GwskZm1jXg8cDvuZJ6/giphy.gif"
+const yesImage ="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExZDBlc3didTY5OHh3dHA4cjdkcDczaTl1ZGswdzg5cWxyZjgzOGYxMyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/n4CZ0c7QjgS9sGAO5A/giphy.gif"
 
 
 function noButtonPressed (){
     const noButton= document.getElementById("noButton")
     const image= document.getElementById("image2")
-    image.src = newimage
+    image.src = noImage
     noButton.innerHTML = noMessages[messageIndex]
     messageIndex = messageIndex + 1 
 }
 
+function yesButtonPressed (){
+    const image= document.getElementById("image2")
+    image.src =yesImage
+
+}
